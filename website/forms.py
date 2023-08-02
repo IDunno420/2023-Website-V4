ed679c1ee720573dc4601ea3bdd7edeeeb4a91a8
IDunno420 <17014@my.sanctamaria.school.nz>
2023-08-03 10:09:55 +1200

    Added a test PNG
    This is to test whether a fix has occured when an error is.

--- website/forms.py
+++ website/forms.py
@@ -25,16 +25,15 @@ class RegistrationForm(FlaskForm):
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators= [FileAllowed('jpg','png')])
-    submit = SubmitField('Sign Up')
+    picture = FileField('Update Profile Picture', validators= [FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('')
-        
-
+    
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
